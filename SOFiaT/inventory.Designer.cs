2ca3d518dbe3de1e1315b48501e854c751df24ab
egsoft18 <edwingmz18@gmail.com>
2018-08-27 11:53:41 -0300

    Se ha agregado la ventana de inventario. Se comenzo a trabajar con el buscador.

---
+++ SOFiaT/inventory.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SOFiaT
+{
+    partial class inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Inventario = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.rbtnidprod = new System.Windows.Forms.RadioButton();
+            this.rbtnname = new System.Windows.Forms.RadioButton();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Inventario
+            // 
+            this.Inventario.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Inventario.AutoSize = true;
+            this.Inventario.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Inventario.Location = new System.Drawing.Point(414, 9);
+            this.Inventario.Name = "Inventario";
+            this.Inventario.Size = new System.Drawing.Size(128, 29);
+            this.Inventario.TabIndex = 0;
+            this.Inventario.Text = "Inventario";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.DodgerBlue;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(741, 258);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // rbtnidprod
+            // 
+            this.rbtnidprod.AutoSize = true;
+            this.rbtnidprod.Location = new System.Drawing.Point(12, 44);
+            this.rbtnidprod.Name = "rbtnidprod";
+            this.rbtnidprod.Size = new System.Drawing.Size(69, 19);
+            this.rbtnidprod.TabIndex = 2;
+            this.rbtnidprod.TabStop = true;
+            this.rbtnidprod.Text = "IDProducto";
+            this.rbtnidprod.UseVisualStyleBackColor = true;
+            // 
+            // rbtnname
+            // 
+            this.rbtnname.AutoSize = true;
+            this.rbtnname.Location = new System.Drawing.Point(93, 44);
+            this.rbtnname.Name = "rbtnname";
+            this.rbtnname.Size = new System.Drawing.Size(61, 19);
+            this.rbtnname.TabIndex = 3;
+            this.rbtnname.TabStop = true;
+            this.rbtnname.Text = "Producto";
+            this.rbtnname.UseVisualStyleBackColor = true;
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtsearch.Font = new System.Drawing.Font("Arial", 8.25F);
+            this.txtsearch.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.txtsearch.Location = new System.Drawing.Point(160, 44);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(331, 20);
+            this.txtsearch.TabIndex = 4;
+            this.txtsearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(765, 339);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.rbtnname);
+            this.Controls.Add(this.rbtnidprod);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Inventario);
+            this.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "inventory";
+            this.Text = "inventory";
+            this.Load += new System.EventHandler(this.inventory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Inventario;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton rbtnidprod;
+        private System.Windows.Forms.RadioButton rbtnname;
+        private System.Windows.Forms.TextBox txtsearch;
+    }
+}
---
+++ SOFiaT/inventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOFiaT
+{
+    public partial class inventory : Form
+    {
+        DBManager c = new DBManager();
+
+        public inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void inventory_Load(object sender, EventArgs e)
+        {
+            string query = "select i.[idproduct] as IDProducto, p.name as Nombre,  sum(i.[entryqty]) as Entrada, sum(i.[outqty]) as Vendidas, sum(i.[entryqty]) - sum(i.[outqty]) as Balance  from inventory as i, products as p where i.idproduct = p.idproduct group by i.idproduct, p.name";
+            c.load_dgv(dataGridView1, query);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
